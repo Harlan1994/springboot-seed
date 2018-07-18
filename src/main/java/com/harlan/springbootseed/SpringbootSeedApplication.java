80a28b91112485cd2f0dded418c245677ef67597
Harlan1994 <1041099438@qq.com>
2018-07-18 18:11:49 +0800

    Initial commit

---
+++ src/main/java/com/harlan/springbootseed/SpringbootSeedApplication.java
@@ -0,0 +1,12 @@
+package com.harlan.springbootseed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSeedApplication.class, args);
+	}
+}
